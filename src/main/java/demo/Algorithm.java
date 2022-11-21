fcd3f52e12eaf75da524811015b982542908dc25
waiting <wujialang1999@163.com>
2022-11-21 16:09:07 +0800

    算法：不同路径

--- src/main/java/demo/Algorithm.java
+++ src/main/java/demo/Algorithm.java
@@ -3011,6 +3011,33 @@ public class Algorithm {
         return list.toArray(new int[list.size()][]);
     }
 
+    /**
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+     * 问总共有多少条不同的路径？
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] tmp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0){
+                    tmp[i][j] = 1;
+                    continue;
+                }
+                if(i - 1 >= 0){
+                    tmp[i][j] += tmp[i - 1][j];
+                }
+                if(j - 1 >= 0){
+                    tmp[i][j] += tmp[i][j - 1];
+                }
+            }
+        }
+        return tmp[m - 1][n - 1];
+    }
+
     public static void main(String[] args) {
         Algorithm algorithm = new Algorithm();
         int[] pushed = new int[]{1,2,3,3};
