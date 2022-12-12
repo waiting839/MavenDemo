e1c5ec6530ddf9dc92abc6a2246006d2e5c9c7e0
waiting <wujialang1999@163.com>
2022-12-12 17:39:21 +0800

    算法：股票最大利润

--- src/main/java/demo/Algorithm.java
+++ src/main/java/demo/Algorithm.java
@@ -3442,6 +3442,25 @@ public class Algorithm {
         flatten_help(root.right, list);
     }
 
+    /**
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     * 输入：[7,1,5,3,6,4]
+     * 输出：5
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int[] prices) {
+        int min = prices[0];
+        int res = 0;
+        for(int i : prices){
+            min = Math.min(min, i);
+            res = Math.max(res, i - min);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Algorithm algorithm = new Algorithm();
         TreeNode node = new TreeNode(1);
