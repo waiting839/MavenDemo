203e289e909afd46862493330c0626306480d6d1
waiting <wujialang1999@163.com>
2023-05-06 17:47:10 +0800

    hot100

--- src/main/java/algorithm/Hot100.java
+++ src/main/java/algorithm/Hot100.java
@@ -2204,6 +2204,29 @@ public class Hot100 {
         return res;
     }
 
+    /**
+     * 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，
+     * 下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+     * @param temperatures
+     * 输入: temperatures = [73,74,75,71,69,72,76,73]
+     * 输出: [1,1,4,2,1,1,0,0]
+     * @return
+     */
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        //单调栈
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int preIndex = stack.pop();
+                res[preIndex] = i - preIndex;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Hot100 hot100 = new Hot100();
         System.out.println(hot100.subarraySum(new int[]{2,0,0}, 2));
