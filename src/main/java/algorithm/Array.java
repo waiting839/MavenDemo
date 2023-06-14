095b8a43d94788ad63d8faecc051218df318bf5c
waiting <wujialang1999@163.com>
2023-06-14 17:34:17 +0800

    数组

--- src/main/java/algorithm/Array.java
+++ src/main/java/algorithm/Array.java
@@ -261,4 +261,79 @@ public class Array {
             j--;
         }
     }
+
+    /**
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+     * 使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+     * 例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+     * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+     * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+     * 输入：nums = [4,5,6,7,0,1,2], target = 0
+     * 输出：4
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //如果nums[mid] >= nums[i]，说明左边是有序的，右边是翻转了
+            if (nums[i] <= nums[mid]) {
+                //如果target在[i,mid]之间，直接j = mid - 1，其他情况就是在后面
+                if (nums[mid] > target && target >= nums[i]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            } else {
+                //这个是右边有序的，左边是翻转的，然后target在[mid,j]直接，直接i = mid + 1
+                if (nums[mid] < target && target <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+     * 如果数组中不存在目标值 target，返回 [-1, -1]。
+     * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+     * 输入：nums = [5,7,7,8,8,10], target = 8
+     * 输出：[3,4]
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                i = mid;
+                j = mid;
+                while (i - 1 >= 0 && nums[i] == nums[i - 1]) {
+                    i = i - 1;
+                }
+                while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                    j = j + 1;
+                }
+                return new int[]{i, j};
+            } else if (nums[mid] > target) {
+                j = mid - 1;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
