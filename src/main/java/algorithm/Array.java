6dbb715027ba1fa80336f456c7ed8bf46cbd83a4
waiting <wujialang1999@163.com>
2023-07-05 18:09:19 +0800

    数组和字符串

--- src/main/java/algorithm/Array.java
+++ src/main/java/algorithm/Array.java
@@ -868,6 +868,42 @@ public class Array {
         return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
     }
 
+    /**
+     * 给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。
+     * 两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+     * s = s1 + s2 + ... + sn
+     * t = t1 + t2 + ... + tm
+     * |n - m| <= 1
+     * 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+     * 注意：a + b 意味着字符串 a 和 b 连接。
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return
+     */
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        int l = s3.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        if (m + n != l) {
+            return false;
+        }
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j<= n; j++) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                }
+                if (j > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
     public static void main(String[] args) {
         Array array = new Array();
         array.generateParenthesis(3);
