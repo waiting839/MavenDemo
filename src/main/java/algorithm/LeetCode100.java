61e8021024827a2a0349894697dc2d0ee3d52a7a
waiting <wujialang1999@163.com>
2023-08-24 15:55:23 +0800

    LeetCode75

--- src/main/java/algorithm/LeetCode100.java
+++ src/main/java/algorithm/LeetCode100.java
@@ -340,6 +340,135 @@ public class LeetCode100 {
         return res;
     }
 
+    /**
+     * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+     * 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+     * 输出：[[1,0,1],[0,0,0],[1,0,1]]
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] mArr = new boolean[m];
+        boolean[] nArr = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    mArr[i] = true;
+                    nArr[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (mArr[i]) {
+                for (int k = 0; k < n; k++) {
+                    matrix[i][k] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (nArr[j]) {
+                for (int k = 0; k < m; k++) {
+                    matrix[k][j] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+     * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     * 输出：[1,2,3,6,9,8,7,4,5]
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0;
+        int down = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while (true) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            if (top > down) {
+                break;
+            }
+            for (int i = top; i <= down; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            if (left > right) {
+                break;
+            }
+            for (int i = right; i >= left; i--) {
+                res.add(matrix[down][i]);
+            }
+            down--;
+            if (top > down) {
+                break;
+            }
+            for (int i = down; i >= top; i--) {
+                res.add(matrix[i][left]);
+            }
+            left++;
+            if (left > right) {
+                break;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+     * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+     * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     * 输出：[[7,4,1],[8,5,2],[9,6,3]]
+     * @param matrix
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - i -1][j];
+                matrix[n - i -1][j] = tmp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+
+    /**
+     * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+     * 每行的元素从左到右升序排列。
+     * 每列的元素从上到下升序排列。
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = matrix.length - 1;
+        int j = 0;
+        while (i >= 0 && j < matrix[0].length) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         LeetCode100 leetCode100 = new LeetCode100();
         leetCode100.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
