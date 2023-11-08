b26769e83ebd1f5617100e12df4e8b57b2c7b6d6
waiting <wujialang1999@163.com>
2023-11-08 18:00:26 +0800

    LeetCode100

--- src/main/java/algorithm/Hot100Again.java
+++ src/main/java/algorithm/Hot100Again.java
@@ -968,9 +968,146 @@ public class Hot100Again {
         return true;
     }
 
+    /**
+     * 在给定的 m x n 网格 grid 中，每个单元格可以有以下三个值之一：
+     * 值 0 代表空单元格；
+     * 值 1 代表新鲜橘子；
+     * 值 2 代表腐烂的橘子。
+     * 每分钟，腐烂的橘子 周围 4 个方向上相邻 的新鲜橘子都会腐烂。
+     * 返回 直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1 。
+     * 输入：grid = [[2,1,1],[1,1,0],[0,1,1]]
+     * 输出：4
+     * @param grid
+     * @return
+     */
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+        int[][] move = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] arr = queue.poll();
+                for (int j = 0; j < 4; j++) {
+                    int x = arr[0] + move[j][0];
+                    int y = arr[1] + move[j][1];
+                    if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        queue.offer(new int[]{x, y});
+                    }
+                }
+            }
+            res++;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return res - 1 < 0 ? 0 : res - 1;
+    }
+
+
+
+    /**
+     * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+     * @param nums
+     * @return
+     */
+    List<List<Integer>> permute_res = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        permute_help(nums, new ArrayList<>());
+        return permute_res;
+    }
+
+    private void permute_help(int[] nums, List<Integer> list) {
+        if (nums.length == list.size()) {
+            permute_res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) {
+                continue;
+            }
+            list.add(nums[i]);
+            permute_help(nums, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    /**
+     * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+     * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+     * 输入：nums = [1,2,3]
+     * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+     * @param nums
+     * @return
+     */
+    List<List<Integer>> subsets_res = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        subsets_help(nums, new ArrayList<>(), 0);
+        return subsets_res;
+    }
+
+    private void subsets_help(int[] nums, List<Integer> list, int start) {
+        subsets_res.add(new ArrayList<>(list));
+        for (int i = start; i < nums.length; i++) {
+            list.add(nums[i]);
+            subsets_help(nums, list, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    /**
+     * 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，
+     * 并以列表形式返回。你可以按 任意顺序 返回这些组合。
+     * candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+     * 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+     * 输入：candidates = [2,3,6,7], target = 7
+     * 输出：[[2,2,3],[7]]
+     * 解释：
+     * 2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+     * 7 也是一个候选， 7 = 7 。
+     * 仅有这两种组合。
+     * @param candidates
+     * @param target
+     * @return
+     */
+    List<List<Integer>> combinationSum_res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        combinationSum_help(candidates, new ArrayList<>(), 0, target, 0);
+        return combinationSum_res;
+    }
+
+    private void combinationSum_help(int[] candidates, List<Integer> list, int sum, int target, int start) {
+        if (sum == target) {
+            combinationSum_res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (sum + candidates[i] > target) {
+                continue;
+            }
+            list.add(candidates[i]);
+            combinationSum_help(candidates, list, sum + candidates[i], target, i);
+            list.remove(list.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         Hot100Again hot100Again = new Hot100Again();
-        hot100Again.findAnagrams("cbaebabacd", "abc");
+        hot100Again.combinationSum(new int[]{2,3,6,7}, 7);
     }
 
     private void swap(int[] nums, int i, int j){
