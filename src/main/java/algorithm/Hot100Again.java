e57b6187d566d67e9a066d7b41ebb4b0b082ad08
waiting <wujialang1999@163.com>
2023-11-16 16:27:35 +0800

    LeetCode100

--- src/main/java/algorithm/Hot100Again.java
+++ src/main/java/algorithm/Hot100Again.java
@@ -1186,9 +1186,116 @@ public class Hot100Again {
         return false;
     }
 
+    /**
+     * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+     * 如果数组中不存在目标值 target，返回 [-1, -1]。
+     * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+     * 输入：nums = [5,7,7,8,8,10], target = 8
+     * 输出：[3,4]
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                int i = mid;
+                int j = mid;
+                while (i > 0 && nums[i - 1] == target) {
+                    i--;
+                }
+                while (j < nums.length - 1 && nums[j + 1] == target) {
+                    j++;
+                }
+                return new int[]{i, j};
+            }else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    /**
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+     * 使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+     * 例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+     * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+     * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+     * 输入：nums = [4,5,6,7,0,1,2], target = 0
+     * 输出：4
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[l]) {
+                if (nums[mid] > target && target >= nums[l]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+     * 若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+     * 若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+     * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+     * 给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+     * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+     * 输入：nums = [3,4,5,1,2]
+     * 输出：1
+     * 解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
+     * @param nums
+     * @return
+     */
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] > nums[r]) {
+                    l = mid + 1;
+                } else {
+                    return nums[l];
+                }
+            }else {
+                if (nums[mid] > nums[mid - 1]) {
+                    r = mid - 1;
+                } else {
+                    return nums[mid];
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Hot100Again hot100Again = new Hot100Again();
-        hot100Again.generateParenthesis(3);
+        hot100Again.searchRange(new int[]{5,7,7,8,8,10}, 9);
     }
 
     private void swap(int[] nums, int i, int j){
