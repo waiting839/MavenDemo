ebf931b12d4a28cc5793dfcfd72f162f987f3aaa
waiting <wujialang1999@163.com>
2023-11-20 14:54:52 +0800

    LeetCode100

--- src/main/java/algorithm/Hot100Again.java
+++ src/main/java/algorithm/Hot100Again.java
@@ -1293,6 +1293,34 @@ public class Hot100Again {
         return -1;
     }
 
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * 有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 每个右括号都有一个对应的相同类型的左括号。
+     * 输入：s = "()"
+     * 输出：true
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if ((c == ')' && stack.peek() == '(') || (c == '}' && stack.peek() == '{') || (c == ']' && stack.peek() == '[')) {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
     public static void main(String[] args) {
         Hot100Again hot100Again = new Hot100Again();
         hot100Again.searchRange(new int[]{5,7,7,8,8,10}, 9);
