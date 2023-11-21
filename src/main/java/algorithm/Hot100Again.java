230039391178b8e8d7f85686cbcd6945def95670
waiting <wujialang1999@163.com>
2023-11-21 17:59:47 +0800

    LeetCode100

--- src/main/java/algorithm/Hot100Again.java
+++ src/main/java/algorithm/Hot100Again.java
@@ -1321,6 +1321,44 @@ public class Hot100Again {
         return stack.isEmpty();
     }
 
+    /**
+     * 给定一个经过编码的字符串，返回它解码后的字符串。
+     * 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+     * 你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+     * 此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+     * 输入：s = "3[a]2[bc]"
+     * 输出："aaabcbc"
+     * 输入：s = "3[a2[c]]"
+     * 输出："accaccacc"
+     * @param s
+     * @return
+     */
+    public String decodeString(String s) {
+        Stack<String> stack = new Stack<>();
+        Stack<Integer> k = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        int m = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                k.push(m);
+                m = 0;
+                stack.push(res.toString());
+                res = new StringBuilder();
+            } else if (c == ']') {
+                StringBuilder tmp = new StringBuilder(stack.pop());
+                for (int i = 0; i < k.pop(); i++) {
+                    tmp.append(res);
+                }
+                res = new StringBuilder(tmp);
+            } else if (c >= '1' && c <= '9') {
+                m = m * 10 + Integer.parseInt(c + "");
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+
     public static void main(String[] args) {
         Hot100Again hot100Again = new Hot100Again();
         hot100Again.searchRange(new int[]{5,7,7,8,8,10}, 9);
